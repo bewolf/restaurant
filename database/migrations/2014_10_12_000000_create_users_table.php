2d78db6b2ecbe216d95f36fcdd9e3492be74ca43
bewolf <georgi.macov@mail.bg>
2019-10-16 16:06:49 +0300

    Move User model to App/Models/User add soft_delete on User model add soft_delete on Create Users table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class  CreateUsersTable extends Migration
 {
     /**
      * Run the migrations.
@@ -16,11 +16,14 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('name');
+            $table->string('username');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
+            $table->softDeletes();
             $table->timestamps();
+
         });
     }
 
