f67cf09dfaa772b15a56e8582e5a31edc044af20
georgi.macov@mail.bg <georgi.macov@mail.bg>
2019-12-20 11:47:38 +0200

    Change gitignore file, add JS files to webpack.mix

--- webpack.mix.js
+++ webpack.mix.js
@@ -13,4 +13,5 @@ const mix = require('laravel-mix');
 
 mix.js('resources/js/app.js', 'public/js')
     .js('resources/js/create_new_order_page', 'public/js')
+    .js('resources/js/create_new_invoice_page', 'public/js')
     .sass('resources/sass/app.scss', 'public/css');
